01e6ea6e6619be99c1092c21a3fe9773e99b0289
johan guzman <johan.dev19@gmail.com>
2016-10-18 12:31:59 -0400

    modificacion de la pagina a un nuevo estilo 2.0

--- index.php
+++ index.php
@@ -1,17 +1,56 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Johan H. Guzman Gil</title>
+        <link rel="stylesheet" href="./css/normalize.css">
+        <link rel="stylesheet" href="./css/font-awesome.min.css">
+        <link rel="stylesheet" href="./css/style.css">
+    </head>
+    <body>
+        <div class="container">
+            <section class="card">
+                <div class="card-avatar">
+                    <figure class="card-avatar_image">
+                        <img src="./img/johan.jpg" alt="imagen de perfil">
+                    </figure>
+                </div>
+                <div class="card-info">
+                    <span class="card-info_mellamo">Me llamo</span>
+                    <strong class="card-info_name">Johan H. Guzman Gil</strong>
+                    <p class="card-info_desc">Programador web front-end y fundador de Medfus</p>
+                    <aside class="card-info-more">
+                        <div class="card-info-more_age">
+                            <span class="card-info_title">Edad</span>
+                            <span>22 años</span>
+                        </div>
+                        <div class="card-info-more_address">
+                            <span class="card-info_title">Dirección</span>
+                            <span>Santo domingo oeste, Republica Dominicana</span>
+                        </div>
+                        <div class="card-info-more_email">
+                            <span class="card-info_title">Email</span>
+                            <span>johan.dev19@gmail.com / johan_ing1@hotmail.com</span>
+                        </div>
+                        <div class="card-info-more_phone">
+                            <span class="card-info_title">Celular</span>
+                            <span>849-632-3447</span>
+                        </div>
+                    </aside>
+                </div>
+            </section>
+            <footer class="card-footer">
+                <a target="_blank" href="https://www.facebook.com/johang18">
+                    <i class="fa fa-facebook-square" aria-hidden="true"></i>
+                </a>
+                <a target="_blank" href="https://twitter.com/johandev19">
+                    <i class="fa fa-twitter-square" aria-hidden="true"></i>
+                </a>
+                <a target="_blank" href="https://www.instagram.com/johang18/">
+                    <i class="fa fa-instagram" aria-hidden="true"></i>
+                </a>
+            </footer>
+        </div>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
-
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+    </body>
+</html>
